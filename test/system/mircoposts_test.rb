8f28d72da941633b9273baacd466e1bc3971b756
Brandon Wieber <brandon.m.wieber@gmail.com>
2017-12-03 16:16:24 -0500

    Basic model connections

---
+++ test/system/mircoposts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class micropostsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit microposts_url
+  #
+  #   assert_selector "h1", text: "micropost"
+  # end
+end
